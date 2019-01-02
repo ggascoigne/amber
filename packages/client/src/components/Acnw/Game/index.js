0f97a17c6c195d89b442500535dd9924eb60e04a
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2019-01-02 11:18:25 -0800

    feat(game): resolve lookup of game preferences

--- packages/client/src/components/Acnw/Game/index.js
+++ packages/client/src/components/Acnw/Game/index.js
@@ -1,1 +1,109 @@
-export * from './Game'
+import { withStyles } from '@material-ui/core'
+import classNames from 'classnames'
+import { Lookup } from 'components/Acnw/Lookup'
+import React from 'react'
+import maskEmail from 'utils/maskEmail'
+import Card from 'components/MaterialKitReact/Card/Card'
+import CardBody from 'components/MaterialKitReact/Card/CardBody'
+import CardHeader from 'components/MaterialKitReact/Card/CardHeader'
+import GridContainer from 'components/MaterialKitReact/Grid/GridContainer'
+import GridItem from 'components/MaterialKitReact/Grid/GridItem'
+
+const styles = {
+  card: {
+    marginBottom: 50
+  },
+  gridItem: {
+    paddingBottom: 10
+  },
+  label: {
+    fontWeight: 500
+  }
+}
+
+const MultiLine = ({ text }) => (
+  <>
+    {text.split('\n').map((i, key) => (
+      <p key={key}>{i}</p>
+    ))}
+  </>
+)
+
+const Field = ({ label, classes, children, small }) => {
+  return (
+    <>
+      <GridItem xs={12} sm={2} className={classNames(classes.gridItem, classes.label)}>
+        {label}
+      </GridItem>
+      <GridItem xs={12} sm={small ? 4 : 10} className={classes.gridItem}>
+        {children}
+      </GridItem>
+    </>
+  )
+}
+
+const _Game = ({ classes, game }) => {
+  const {
+    id,
+    name,
+    slotId = 0,
+    description,
+    charInstructions,
+    gameContactEmail,
+    genre,
+    playerMax,
+    playerMin,
+    playerPreference,
+    playersContactGm,
+    type,
+    teenFriendly,
+    gameAssignments: gms,
+    setting
+  } = game
+  return slotId ? (
+    <Card key={`game_${id}`} className={classes.card}>
+      <CardHeader color='info'>
+        <h4>{name}</h4>
+      </CardHeader>
+      <CardBody>
+        <GridContainer>
+          <Field label={'Game Master'} classes={classes}>
+            {gms.nodes.map(a => a.member.user.profile.fullName).join(', ')}
+          </Field>
+          <Field label={'Description'} classes={classes}>
+            <MultiLine text={description} />
+          </Field>
+          {setting && (
+            <Field label={'Setting'} classes={classes}>
+              <MultiLine text={setting} />
+            </Field>
+          )}
+          {charInstructions && (
+            <Field label={'Character & Player Instructions'} classes={classes}>
+              <MultiLine text={charInstructions} />
+            </Field>
+          )}
+          <Field label={'Genre/Type'} classes={classes} small>
+            {genre} - {type}
+          </Field>
+          <Field label={'Teen Friendly'} classes={classes} small>
+            {teenFriendly ? 'Yes' : 'No'}
+          </Field>
+          <Field label={'Number of Players'} classes={classes} small>
+            {playerMin} - {playerMax}
+          </Field>
+          <Field label={'Player Preference'} classes={classes} small>
+            <Lookup realm={'gamePlayerPref'} code={playerPreference} />
+          </Field>
+          <Field label={''} classes={classes}>
+            {playersContactGm
+              ? `Players should contact the GM at '${maskEmail(gameContactEmail)}' prior to the convention.`
+              : `Players need not contact the GM in advance of the convention.`}
+          </Field>
+        </GridContainer>
+      </CardBody>
+    </Card>
+  ) : null
+}
+
+export const Game = withStyles(styles, { withTheme: true })(_Game)
---
+++ packages/client/src/components/Acnw/Lookup/index.js
@@ -0,0 +1,42 @@
+import gql from 'graphql-tag'
+import get from 'lodash/get'
+import React from 'react'
+import { Query } from 'react-apollo'
+import { GraphQLError } from '../GraphQLError'
+import { Loader } from '../Loader'
+
+const QUERY_LOOKUP = gql`
+  query($realm: String!, $code: String!) {
+    lookups(condition: { realm: $realm }) {
+      edges {
+        node {
+          realm
+          lookupValues(condition: { code: $code }) {
+            nodes {
+              id
+              code
+              sequencer
+              value
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export const Lookup = ({ realm, code }) => {
+  return (
+    <Query query={QUERY_LOOKUP} variables={{ realm, code }} errorPolicy='all'>
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <Loader />
+        }
+        if (error) {
+          return <GraphQLError error={error} />
+        }
+        return get(data, 'lookups.edges[0].node.lookupValues.nodes[0].value')
+      }}
+    </Query>
+  )
+}
