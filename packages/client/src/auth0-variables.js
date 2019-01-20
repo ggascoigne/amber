38e5275db9c15fc49c19f64610d837c13214edf8
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2019-01-19 19:12:58 -0800

    feat(dev): fix auth0 callback url

--- packages/client/src/auth0-variables.js
+++ packages/client/src/auth0-variables.js
@@ -2,6 +2,6 @@ export const AUTH_CONFIG = {
   domain: 'wyrdrune-test.auth0.com',
   roleUrl: 'https://amberconnw.org/role',
   clientId: 'RuExP1gZQsfGLl3ImMwhJo8n0PwNf2PP',
-  callbackUrl: 'http://localhost:3000/callback',
-  logoutUrl: 'http://localhost:3000/'
+  callbackUrl: 'http://localhost:30000/callback',
+  logoutUrl: 'http://localhost:30000/'
 }
