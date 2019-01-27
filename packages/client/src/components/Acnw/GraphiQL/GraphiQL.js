8e0eaed9a5e5348b38334f41e5e83f8072eb24a8
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2019-01-26 16:58:30 -0800

    feat(graphiql): add GraphiQL Explorer
    Add https://github.com/OneGraph/graphiql-explorer, based on the example at https://github.com/OneGraph/graphiql-explorer-example

--- packages/client/src/components/Acnw/GraphiQL/GraphiQL.js
+++ packages/client/src/components/Acnw/GraphiQL/GraphiQL.js
@@ -2,15 +2,32 @@ import withStyles from '@material-ui/core/styles/withStyles'
 import classNames from 'classnames'
 import { Page } from 'components/Acnw/Page'
 import RealGraphiQL from 'graphiql'
+import GraphiQLExplorer from 'graphiql-explorer'
 import 'graphiql/graphiql.css'
-import { isLeafType, isNonNullType } from 'graphql'
+import { buildClientSchema, getIntrospectionQuery } from 'graphql'
 import fetch from 'isomorphic-fetch'
-import React from 'react'
+import React, { Component } from 'react'
+import { getDefaultScalarArgValue, makeDefaultArg } from './CustomArgs'
 
 const styles = theme => ({
   graphiQlWrapper: {
+    height: 'calc(100% - 85px) !Important',
+    maxHeight: '100vh'
+  },
+  box: {
     boxSizing: 'content-box',
-    height: 'calc(100% - 112px)'
+    border: '1px solid #d6d6d6',
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  '@global': {
+    '.graphiql-explorer-root': {
+      overflow: 'unset !important'
+    },
+    '.graphiql-container .historyPaneWrap': {
+      width: '300px !important',
+      boxShadow: 'none !important'
+    }
   }
 })
 
@@ -22,39 +39,64 @@ function graphQLFetcher(graphQLParams) {
   }).then(response => response.json())
 }
 
-function getDefaultFieldNames(type) {
-  // If this type cannot access fields, then return an empty set.
-  if (!type.getFields) {
-    return []
+class GraphiQL extends Component {
+  _graphiql
+  state = { schema: null, query: '', explorerIsOpen: true }
+
+  componentDidMount() {
+    graphQLFetcher({
+      query: getIntrospectionQuery()
+    }).then(result => {
+      this.setState({ schema: buildClientSchema(result.data) })
+    })
   }
 
-  const fields = type.getFields()
-  if (fields['edges']) {
-    return ['edges']
-  }
-  if (fields['node']) {
-    return ['node']
+  _handleEditQuery = query => this.setState({ query })
+
+  _handleToggleExplorer = () => {
+    this.setState({ explorerIsOpen: !this.state.explorerIsOpen })
   }
 
-  // Include all leaf-type fields.
-  const leafFieldNames = []
-  Object.keys(fields).forEach(fieldName => {
-    if (
-      isLeafType(fields[fieldName].type) ||
-      (isNonNullType(fields[fieldName].type) && isLeafType(fields[fieldName].type.ofType))
-    ) {
-      leafFieldNames.push(fieldName)
-    }
-  })
-  return leafFieldNames
-}
-
-const GraphiQL = ({ classes }) => {
-  return (
-    <Page className={classNames(classes.graphiQlWrapper)}>
-      <RealGraphiQL fetcher={graphQLFetcher} getDefaultFieldNames={getDefaultFieldNames} />
-    </Page>
-  )
+  render() {
+    const { classes } = this.props
+    const { query, schema } = this.state
+    return (
+      <Page className={classNames(classes.graphiQlWrapper)}>
+        <div className={classNames(classes.box, 'graphiql-container')}>
+          <GraphiQLExplorer
+            schema={schema}
+            query={query}
+            onEdit={this._handleEditQuery}
+            explorerIsOpen={this.state.explorerIsOpen}
+            onToggleExplorer={this._handleToggleExplorer}
+            getDefaultScalarArgValue={getDefaultScalarArgValue}
+            makeDefaultArg={makeDefaultArg}
+          />
+          <RealGraphiQL
+            ref={ref => (this._graphiql = ref)}
+            fetcher={graphQLFetcher}
+            schema={schema}
+            query={query}
+            onEditQuery={this._handleEditQuery}
+          >
+            <RealGraphiQL.Toolbar>
+              <RealGraphiQL.Button
+                onClick={() => this._graphiql.handlePrettifyQuery()}
+                label='Prettify'
+                title='Prettify Query (Shift-Ctrl-P)'
+              />
+              <RealGraphiQL.Button
+                onClick={() => this._graphiql.handleToggleHistory()}
+                label='History'
+                title='Show History'
+              />
+              <RealGraphiQL.Button onClick={this._handleToggleExplorer} label='Explorer' title='Toggle Explorer' />
+            </RealGraphiQL.Toolbar>
+          </RealGraphiQL>
+        </div>
+      </Page>
+    )
+  }
 }
 
 // note that this needs to match what is expected by GraphiQLPage
