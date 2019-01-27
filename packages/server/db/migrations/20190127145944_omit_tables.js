5e8e7137338cbd7ee7411c23aa15ab4e87769e0c
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2019-01-27 15:10:19 -0800

    feat(gql): hide knex migration from postgraphile

---
+++ packages/server/db/migrations/20190127145944_omit_tables.js
@@ -0,0 +1,8 @@
+exports.up = async function(knex) {
+  await knex.raw(`
+    comment on table knex_migrations is E'@omit';
+    comment on table knex_migrations_lock is E'@omit';
+  `)
+}
+
+exports.down = async function(knex) {}
