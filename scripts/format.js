abcd06b2b74db7ad60bca05bbddb5f9f89055b73
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2019-02-09 12:21:15 -0800

    fix(reformat): make reformat code path finding more reliable

--- scripts/format.js
+++ scripts/format.js
@@ -2,13 +2,16 @@
 const fs = require('fs')
 const execa = require('execa')
 const meow = require('meow')
+const path = require('path')
 
-// Reformat a single file using `import-sort | prettier`
-// designed to be called from a file change watcher from inside IntellliJ IDEA
-// since there seems to be a race condition calling two separate file watchers
-// on the same file, and it appears to be rather random which one generates the
-// output. By explicitly running each piping the output of the first into the
-// second seems to be pretty reliable.
+const BIN_DIRNAME = path.resolve(__dirname, '../node_modules/.bin')
+
+// Reformat a single file using `import-sort | prettier`, matching our preferred code style
+//
+// This script is designed to be called from a file change watcher from inside IntellliJ IDEA. Without it, there seems
+// to be a race condition calling two separate file watchers on the same file, and it appears to be rather random which
+// one generates the output. By explicitly running each, piping the output of the first into the second seems to be
+// pretty reliable.
 
 const cli = meow(
   `
@@ -28,14 +31,15 @@ void (async function go() {
   !filename && cli.showHelp()
 
   // note that this really only works on *.{js,jsx,ts,tsx} files, others error out but don't change the input
+
   // todo clean up error handling
   // todo pass other supported file types through to prettier
 
-  const sorter = execa('./node_modules/.bin/import-sort', [filename], {
+  const sorter = execa(`${BIN_DIRNAME}/import-sort`, [filename], {
     stdin: 'inherit'
   })
 
-  const prettier = execa('./node_modules/.bin/prettier', ['--stdin', '--stdin-filepath', filename], {
+  const prettier = execa(`${BIN_DIRNAME}/prettier`, ['--stdin', '--stdin-filepath', filename], {
     stripFinalNewline: false
   })
   sorter.stdout.pipe(prettier.stdin)
