66e8c2e39bb3b749efba05523f208cd01781a8a1
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2019-03-30 16:27:13 -0700

    chore(all): library updates

--- packages/ui/src/utils/wrapInRouter.js
+++ packages/ui/src/utils/wrapInRouter.js
@@ -1,8 +1,8 @@
-import createHistory from 'history/createBrowserHistory'
+import { createBrowserHistory } from 'history'
 import React from 'react'
 import { Router } from 'react-router-dom'
 
 // used in tests only
-const wrapInRouter = child => <Router history={createHistory()}>{child}</Router>
+const wrapInRouter = child => <Router history={createBrowserHistory()}>{child}</Router>
 
 export default wrapInRouter
