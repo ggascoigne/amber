dc21b4e2c58a17e821c42b5e64eecf69d9bbe01e
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2020-04-17 14:10:41 -0700

    feat(build): ensure env vars pass through to build

--- support/knexfile.js
+++ support/knexfile.js
@@ -1,31 +1,18 @@
 process.env.NODE_ENV !== 'production' && require('dotenv').config({ path: '../.env' })
 
-const config = {
-  database: {
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD || '',
-    port: parseInt(process.env.DATABASE_PORT, 10),
-    ssl: process.env.DATABASE_SSL === '1',
-    ssl_cert: process.env.DATABASE_SSL_CERT || ''
-  }
-}
-
-module.exports = {
+const knexConfig = {
   migrations: {
     tableName: 'knex_migrations',
     directory: './db/migrations'
   },
   client: 'pg',
-  debug: config.debug,
   connection: {
-    host: config.database.host,
-    port: config.database.port,
-    user: config.database.username,
-    password: config.database.password,
-    database: config.database.database,
-    ssl: config.database.ssl,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD || '',
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    ssl: process.env.DATABASE_SSL === '1',
     charset: 'utf8',
     dateStrings: true,
     timezone: 'UTC'
@@ -33,4 +20,7 @@ module.exports = {
   acquireConnectionTimeout: 5000
 }
 
-console.log(JSON.stringify(module.exports))
+module.exports = knexConfig
+
+const config = knexConfig.connection
+console.log(`using: postgres://${config.user}:*****@${config.host}:${config.port}/${config.database}`)
