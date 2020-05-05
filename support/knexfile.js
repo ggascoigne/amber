9ca642381facaf9de5e6bf953d3b764b87346e1a
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2020-05-04 22:32:28 -0700

    fix(build): update for new pg lib

--- support/knexfile.js
+++ support/knexfile.js
@@ -1,9 +1,11 @@
 process.env.NODE_ENV !== 'production' && require('dotenv').config({ path: '.env' })
 
+const fs = require('fs')
+
 const knexConfig = {
   migrations: {
     tableName: 'knex_migrations',
-    directory: './db/migrations'
+    directory: './db/migrations',
   },
   client: 'pg',
   connection: {
@@ -12,12 +14,19 @@ const knexConfig = {
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD || '',
     port: parseInt(process.env.DATABASE_PORT, 10),
-    ssl: process.env.DATABASE_SSL === '1',
+    ssl:
+      process.env.DATABASE_SSL === '1'
+        ? {
+            rejectUnauthorized: true,
+            sslmode: 'verify-all',
+            ca: fs.readFileSync('./shared/' + process.env.DATABASE_SSL_CERT || '').toString(),
+          }
+        : false,
     charset: 'utf8',
     dateStrings: true,
-    timezone: 'UTC'
+    timezone: 'UTC',
   },
-  acquireConnectionTimeout: 5000
+  acquireConnectionTimeout: 5000,
 }
 
 module.exports = knexConfig
