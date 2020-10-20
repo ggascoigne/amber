01b4ed8110589bd968554b20a63d2f978e16e274
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2020-10-20 11:38:33 -0700

    library upgrades

---
+++ src/utils/pick.test.ts
@@ -0,0 +1,21 @@
+import { omit, pick } from './pick'
+
+const person = {
+  name: 'Jeff',
+  dog: 'Daffodil',
+  cat: 'Phoebe',
+}
+
+test('pick', () => {
+  expect(pick(person, 'name', 'dog')).toStrictEqual({
+    name: 'Jeff',
+    dog: 'Daffodil',
+  })
+})
+
+test('omit', () => {
+  expect(omit(person, 'dog')).toStrictEqual({
+    name: 'Jeff',
+    cat: 'Phoebe',
+  })
+})
--- src/utils/pick.ts
+++ src/utils/pick.ts
@@ -2,3 +2,16 @@
 export function pick<T, D extends keyof T>(o: T, ...props: D[]) {
   return Object.assign({}, ...props.map((prop) => ({ [prop]: o[prop] })))
 }
+
+// export function pick2<T, D extends keyof T>(obj: T, ...props: D[]) {
+//   return props.reduce((result, prop) => {
+//     result[prop] = obj[prop]
+//     return result
+//   }, {} as T)
+// }
+
+export function omit<T, D extends keyof T>(obj: T, ...props: D[]) {
+  const result = { ...obj }
+  props.forEach((prop) => delete result[prop])
+  return result
+}
