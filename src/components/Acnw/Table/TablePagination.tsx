3bae9c2bcb0f80ddd3cb57cff17d5533be0acefd
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2021-06-02 09:10:14 -0700

    add goto first & last pages to the table pagination control

--- src/components/Acnw/Table/TablePagination.tsx
+++ src/components/Acnw/Table/TablePagination.tsx
@@ -1,6 +1,75 @@
 import { TablePagination as MuiTablePagination } from '@material-ui/core'
+import IconButton from '@material-ui/core/IconButton'
+import { Theme, createStyles, makeStyles, useTheme } from '@material-ui/core/styles'
+import FirstPageIcon from '@material-ui/icons/FirstPage'
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft'
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
+import LastPageIcon from '@material-ui/icons/LastPage'
 import React, { PropsWithChildren, ReactElement, useCallback } from 'react'
 import type { TableInstance } from 'react-table'
+
+const useStyles1 = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexShrink: 0,
+      marginLeft: theme.spacing(2.5),
+    },
+  })
+)
+
+interface TablePaginationActionsProps {
+  count: number
+  page: number
+  rowsPerPage: number
+  onChangePage: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void
+}
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const classes = useStyles1()
+  const theme = useTheme()
+  const { count, page, rowsPerPage, onChangePage } = props
+
+  const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onChangePage(event, 0)
+  }
+
+  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onChangePage(event, page - 1)
+  }
+
+  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onChangePage(event, page + 1)
+  }
+
+  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1))
+  }
+
+  return (
+    <div className={classes.root}>
+      <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label='first page'>
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label='previous page'>
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label='next page'
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label='last page'
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  )
+}
 
 export function TablePagination<T extends Record<string, unknown>>({
   instance,
@@ -37,6 +106,7 @@ export function TablePagination<T extends Record<string, unknown>>({
       onChangeRowsPerPage={(e) => {
         setPageSize(Number(e.target.value))
       }}
+      ActionsComponent={TablePaginationActions}
     />
   ) : null
 }
