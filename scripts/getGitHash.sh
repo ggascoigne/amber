4fceb76871a00b1dd3444be562ccb747e584f019
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2021-07-29 10:17:33 -0700

    fix getGitHash.sh to work outside of repo

--- scripts/getGitHash.sh
+++ scripts/getGitHash.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-echo "export const gitHash=$(git show --no-patch --no-notes --pretty='{ hash: "%H", date: "%cI" }' HEAD)" > src/version.ts
+FILE=src/version.ts
+if [ -d .git ]
+then
+  val=$(git show --no-patch --no-notes --pretty='{ hash: "%H", date: "%cI" }' HEAD)
+else
+  d=$(date -Iseconds)
+  val="{ hash: \"development\", date: \"$d\" }"
+fi
+echo "export const gitHash=$val" > $FILE
