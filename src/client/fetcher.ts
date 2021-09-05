2e5b9c6b7cf8759e4c7d67113b5df16320d51951
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2021-09-04 20:35:15 -0700

    append the graphql operation name to the fet5ch url - makes debugging much easier

--- src/client/fetcher.ts
+++ src/client/fetcher.ts
@@ -1,6 +1,7 @@
 import { useAuth } from 'components/Auth'
 import { print } from 'graphql'
 import gql from 'graphql-tag'
+import { OperationDefinitionNode } from 'graphql/language/ast'
 
 // eslint-disable-next-line etc/no-misused-generics
 export const useFetchData = <TData, TVariables>(query: string): (() => Promise<TData>) => {
@@ -16,16 +17,19 @@ export const useFetchData = <TData, TVariables>(query: string): (() => Promise<T
       }
     }
 
+    const ast = gql(query)
+    const operation = ast.definitions.find((def) => def.kind === 'OperationDefinition') as OperationDefinitionNode
+    const operationName = operation?.name?.value ?? ''
     // note that we use gql from graphql-tag to drop duplicate fragment definitions
     // then we use graphql print to convert that back to a text string.
-    const res = await fetch('/api/graphql', {
+    const res = await fetch(`/api/graphql/${operationName}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         ...headers,
       },
       body: JSON.stringify({
-        query: print(gql(query)),
+        query: print(ast),
         variables,
       }),
     })
