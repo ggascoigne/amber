ef258539a9079d84110d0ee01b6986fa66aeba5b
Edwin Voskamp <github.com@voskamp.org>
2023-01-06 19:50:30 -0800

    Explain the new auth system more detailedly
    Added explanatory text with the important bits bolded.
    
    Added a link to the FAQ.
    
    Added step-by-step instructions on what to do.
    
    Resolves: #41

--- apps/acus/views/Memberships/BecomeAMember.tsx
+++ apps/acus/views/Memberships/BecomeAMember.tsx
@@ -44,24 +44,37 @@ export const BecomeAMember = () => {
               Attending <span style={{ color: theme.palette.error.main }}>virtual</span> {configuration.title}
             </h2>
           ) : (
-            <h2>Attending {configuration.title}</h2>
+            <h2>Logging In to this {configuration.title} Site</h2>
           )}
           <IsNotLoggedIn>
             {!disableLogin ? (
               <>
-                <h4>New Authentication system.</h4>
                 <p>
-                  We have a new authentication system. If you have never logged into the new site, you have to sign up
-                  for a userid / password with auth0. Click on "LOGIN / SIGN UP" and on the screen, click on the little
-                  blue link "Sign up" at the bottom of the white popup box. You will get a verification email: you have
-                  to verify the email you use, sign out from the Ambercon website, and sign back in.
+                  We have a new authentication system.{' '}
+                  <strong>Your userid and password from before 2023 are gone</strong>. You have to{' '}
+                  <strong>create a new userid and password</strong>. You can{' '}
+                  <strong>re-create your userid with the same email address</strong>. For more information see our{' '}
+                  <a href='/faq'>Frequently Asked Questions</a>.
                 </p>
-                <p>
-                  If you had an account for a previous Ambercon, you can link back to it by signing up again using the
-                  same email address as before and then confirming that email address. If you are a GM, this will give
-                  you option of copying games forward. If you don't know what email you used, please contact us at
-                  signup@ambercon.com.
-                </p>
+                <h4>Creating a Userid and Password</h4>
+                <ol>
+                  <li>Click on "LOGIN / SIGN UP"</li>
+                  <li>On the white popup log in screen, click on the little blue link "Sign up" at the bottom</li>
+                  <li>
+                    Create your userid. <b>If you've used the Ambercon site since 2009, use the same email address</b>.
+                    If you had an account for a previous Ambercon, you can link back to it by signing up again using the
+                    same email address as before and then confirming that email address. If you are a GM, this will give
+                    you option of copying games forward. If you don't know what email you used, please contact us at
+                    <a href='mailto:signup@ambercon.com'>signup@ambercon.com</a>.
+                  </li>
+                  <li>Though it logs you in, nothing will work until your email address is verified.</li>
+                  <li>
+                    You will get a <strong>verification email</strong>
+                  </li>
+                  <li>Follow the instructions in the email (click on a link)</li>
+                  <li>Sign out from the Ambercon website</li>
+                  <li>Sign back in</li>
+                </ol>
                 <Button
                   variant='outlined'
                   color='primary'
