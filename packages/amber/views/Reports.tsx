5d34e82c4d16ff6fa0bc2422f9e0e291f9e0a314
Edwin Voskamp <github.com@voskamp.org>
2023-01-08 19:53:41 -0800

    Refactor into a data-driven generate list of reports
    Modified to define an array of objects:
     -  name: string
        used (CSS UPPERCASED) in the button as "Download Label Report"
     -  url?: string
        if not set, it uses lowercased name as api/reports/nameReport
     -  filelabel?: string
        if not set, it uses lowercased name
     -  apps?: string[]
        array of strings of configuration abbreviations to show this for
        if not set the link is generated
        if set if configuration.abbr appears in the array, it is
        generated
     - virtua?: boolean
       if not set the link is always generated
       if set, if configuration.virtual matches it, it is generated
    
    Resolves: #60

--- packages/amber/views/Reports.tsx
+++ packages/amber/views/Reports.tsx
@@ -5,74 +5,48 @@ import { useConfiguration } from '../utils'
 
 const Reports = () => {
   const configuration = useConfiguration()
+  // name is used in title case in the button as "Download Label Report"
+  // if url is not set, it uses lowercased name as api/reports/nameReport
+  // if filelabel is not set, it uses name
+  // if apps is not set it is always generated, otherwise only for matching configuration.abbr
+  // if virtual is not set it is always generated, if set, only if matching configuration.virtual
+  type ReportRecord = { name: string; url?: string; filelabel?: string; apps?: string[]; virtual?: boolean }
+  const reports: ReportRecord[] = [
+    { name: 'Membership' },
+    { name: 'Game' },
+    { name: 'Discord Game', url: 'gameReportForDiscord', filelabel: 'gamesDiscord', virtual: true },
+    { name: 'GM' },
+    { name: 'Game And Players', url: 'gamesAndPlayers', filelabel: 'gamesAndPlayers' },
+    { name: 'Room Usage', url: 'roomReport', filelabel: 'room', apps: ['acnw'] },
+    { name: 'Gamescheduler', apps: ['acus'] },
+  ]
   const timestamp = new Date().toISOString().replaceAll('-', '_').replaceAll('.', '_')
+
   return (
     <Page title='Reports'>
       <List>
-        <ListItem>
-          <AuthenticatedDownloadButton
-            url='/api/reports/membershipReport'
-            filename={`${configuration.abbr.toUpperCase()}${configuration.year}-membership-${timestamp}.xlsx`}
-          >
-            Download Membership Report
-          </AuthenticatedDownloadButton>
-        </ListItem>
-        <ListItem>
-          <AuthenticatedDownloadButton
-            url='/api/reports/gameReport'
-            filename={`${configuration.abbr.toUpperCase()}${configuration.year}-games-${timestamp}.xlsx`}
-          >
-            Download Game Report
-          </AuthenticatedDownloadButton>
-        </ListItem>
-        {configuration.virtual && (
-          <ListItem>
-            <AuthenticatedDownloadButton
-              url='/api/reports/gameReportForDiscord'
-              filename={`${configuration.abbr.toUpperCase()}${configuration.year}-gamesDiscord-${timestamp}.xlsx`}
-            >
-              Download Discord Game Report
-            </AuthenticatedDownloadButton>
-          </ListItem>
-        )}
-        <ListItem>
-          <AuthenticatedDownloadButton
-            url='/api/reports/gmReport'
-            filename={`${configuration.abbr.toUpperCase()}${configuration.year}-gm-${timestamp}.xlsx`}
-          >
-            Download GM Report
-          </AuthenticatedDownloadButton>
-        </ListItem>
-        <ListItem>
-          <AuthenticatedDownloadButton
-            url='/api/reports/gamesAndPlayers'
-            filename={`${configuration.abbr.toUpperCase()}${configuration.year}-gamesAndPlayers-${timestamp}.xlsx`}
-          >
-            Download Game and Players Report
-          </AuthenticatedDownloadButton>
-        </ListItem>
-        {configuration.abbr === 'acnw' && (
-          <ListItem>
-            <AuthenticatedDownloadButton
-              url='/api/reports/roomReport'
-              filename={`${configuration.abbr.toUpperCase()}${configuration.year}-roomReport-${timestamp}.xlsx`}
-            >
-              Download Room Usage Report
-            </AuthenticatedDownloadButton>
-          </ListItem>
-        )}
-        {configuration.abbr === 'acus' && (
-          <ListItem>
-            <AuthenticatedDownloadButton
-              url='/api/reports/gameschedulerReport'
-              filename={`${configuration.abbr.toUpperCase()}${
-                configuration.year
-              }-gameschedulerReport-${timestamp}.xlsx`}
-            >
-              Download Game Scheduler Report
-            </AuthenticatedDownloadButton>
-          </ListItem>
-        )}
+        {reports.map((r) => {
+          if (r.apps !== undefined && !r.apps.includes(configuration.abbr)) {
+            return <></>
+          }
+          if (r.virtual !== undefined && r.virtual !== configuration.virtual) {
+            return <></>
+          }
+          const filelabel = r.filelabel !== undefined ? r.filelabel : r.name.toLowerCase()
+          const url = r.url !== undefined ? r.url : `${r.name.toLowerCase()}Report`
+          console.log(r.name)
+          console.log(url)
+          return (
+            <ListItem>
+              <AuthenticatedDownloadButton
+                url={`/api/reports/${url}`}
+                filename={`${configuration.abbr.toUpperCase()}${configuration.year}-${filelabel}-${timestamp}.xlsx`}
+              >
+                Download {r.name} Report
+              </AuthenticatedDownloadButton>
+            </ListItem>
+          )
+        })}
       </List>
     </Page>
   )
