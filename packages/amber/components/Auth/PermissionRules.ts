8dc3deb144ede8c4fd7e4e20d50683c736a38298
Edwin Voskamp <github.com@voskamp.org>
2023-02-18 19:01:11 -0800

    Add PlayerAdmin as a role

--- packages/amber/components/Auth/PermissionRules.ts
+++ packages/amber/components/Auth/PermissionRules.ts
@@ -7,7 +7,15 @@ export type PermissionDeclaration = AtLeastOne<{
 
 export type Rules = Record<string, PermissionDeclaration>
 
-export const Perms = asEnumLike(['GraphiqlLoad', 'IsAdmin', 'IsLoggedIn', 'FullGameBook', 'GameAdmin', 'Reports'])
+export const Perms = asEnumLike([
+  'GraphiqlLoad',
+  'IsAdmin',
+  'IsLoggedIn',
+  'FullGameBook',
+  'GameAdmin',
+  'PlayerAdmin',
+  'Reports',
+])
 
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Perms = keyof typeof Perms
@@ -20,6 +28,10 @@ const rules = {
   },
   ROLE_GAME_ADMIN: {
     static: [Perms.FullGameBook, Perms.GameAdmin, Perms.Reports, Perms.IsLoggedIn],
+  },
+  // INSERT INTO ROLE (id, authority) VALUES(6,'ROLE_PLAYER_ADMIN')
+  ROLE_PLAYER_ADMIN: {
+    static: [Perms.FullGameBook, Perms.PlayerAdmin, Perms.Reports, Perms.IsLoggedIn],
   },
   ROLE_REPORTS: {
     static: [Perms.Reports, Perms.IsLoggedIn],
