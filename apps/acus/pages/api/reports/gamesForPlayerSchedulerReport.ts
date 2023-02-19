eb7d376d6efe0413848dfa88662b54d585d436e3
Edwin Voskamp <github.com@voskamp.org>
2023-02-19 10:26:57 -0800

    Add reports to match old playerscheduler exports
    Rename game choices report to match pattern 'ForPlayerScheduler'
    Add report for members to match values and columns
    Add report for games to match values and columns
    
    If you use an issue tracker, put references to them at the bottom,
    like this:
    
    Resolves: #76, #77

---
+++ apps/acus/pages/api/reports/gamesForPlayerSchedulerReport.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { configuration } from '../_constants'
+import { handleError } from '../_handleError'
+import { queryToExcelDownload } from './_queryToExcelDownload'
+
+// /api/send/gameReport
+// auth token: required
+// body: {
+//  year?: number
+// }
+
+export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const year = req.body?.year || configuration.year
+    const query = `
+      select 
+        g.id as "NID",
+        g.year as "Con",
+        g.slot_id as "Slot",
+        REPLACE(REPLACE(REPLACE(g.player_preference,'ret-only','Returning Players Only'),'ret-pref','Open Spaces'),'any','Open Spaces') as "Space",
+        g.name as "Title",
+        g.gm_names as "GM(s)",
+        REGEXP_REPLACE(g.gm_names, '\n', ' & ') as "GM(s)",
+        REGEXP_REPLACE(g.gm_names, '\n', ',') as "GMUserNames",
+        g.description as "Description",
+        (case when g.teen_friendly then 'Yes' else 'No' end) as "Teen",
+        REPLACE(REPLACE(REPLACE(g.player_preference,'ret-only','Returning Players Only'),'ret-pref','Returning Players Given Preference, New Players Welcome'),'any','Any (Returning Players not given preference)') as "Accepted",
+        g.player_min as "Min",
+        g.player_max as "Max",
+        g.returning_players as "Returning Players"
+      from game g join "user" u on g.author_id = u.id
+      where g.year = ${year} and g.slot_id >=1 and g.slot_id <= 8;`
+    await queryToExcelDownload(query, res)
+  } catch (err: any) {
+    handleError(err, res)
+  }
+})
---
+++ apps/acus/pages/api/reports/membersForPlayerSchedulerReport.ts
@@ -0,0 +1,49 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { configuration } from '../_constants'
+import { handleError } from '../_handleError'
+import { queryToExcelDownload } from './_queryToExcelDownload'
+
+// /api/send/membershipReport
+// auth token: required
+// body: {
+//  year?: number
+// }
+
+export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const year = req.body?.year || configuration.year
+    const query = `
+      select 
+        u.display_name as "Common Name",
+        u.display_name as "Display Name",
+        u.full_name as "Full Name",
+        u.first_name as "First Name",
+        u.last_name as "Last Name",
+        REPLACE(u.display_name, u.display_name, '') as "Phone Number",
+        u.id as "Username",
+        u.email as "E-mail",
+        mc.times_attending as "Times Attending",
+        coalesce(gm.isGm,false) as "Is GM",
+        m.message as "MMessage",
+        gs.message as "GSMessage",
+        u.id as "Uid"
+        from membership m 
+        join "user" u on m.user_id = u.id 
+        left join "game_submission" gs on m.id = gs.member_id
+        left join (select distinct m.id, true as isGm
+          from membership m
+          join game_assignment ga on ga.member_id = m.id
+          where m.year = ${year} and ga.gm < 0
+        ) gm on gm.id = m.id
+        join hotel_room h on m.hotel_room_id = h.id
+        left join (select m.user_id, count(m.user_id) as times_attending 
+        from membership m 
+        group by m.user_id
+        ) mc on u.id = mc.user_id
+    where m.year = ${year};`
+    await queryToExcelDownload(query, res)
+  } catch (err: any) {
+    handleError(err, res)
+  }
+})
