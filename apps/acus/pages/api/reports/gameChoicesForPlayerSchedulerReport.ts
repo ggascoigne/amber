8279c2b1c183caa01c2a1355d0fa0bba5e2f58a1
Edwin Voskamp <35160149+evoskamp@users.noreply.github.com>
2023-02-23 10:18:00 -0800

    ACUS: Include special games and format returning player in game choices report for player scheduler (#90)
    * Include special games and format returning player
    
    The report joined from game to slot and the special game Any Game is
    in no specific slot so dropped out.
    
    Special games also have no year - they are reused - so are included
    by special magic ids.
    
    The returning player was formatted to Yes/No for space reasons in the
    scheduling spreadsheet.
    
    Resolves: #89
    
    * Remove hardcoded year from query over using variable
    
    Resolves: #89

--- apps/acus/pages/api/reports/gameChoicesForPlayerSchedulerReport.ts
+++ apps/acus/pages/api/reports/gameChoicesForPlayerSchedulerReport.ts
@@ -25,15 +25,15 @@ export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiRespo
         gc.rank as "Choice", 
         g.name as "Game Title",
         g.gm_names as "GM Names",
-        gc.returning_player as "Returning Player",
+        (case when gc.returning_player then 'Yes' else 'No' end) as "Returning Player",
         gs.message as "Message"
       from game_choice gc 
         join game g on gc.game_id = g.id 
-        join slot s on g.slot_id = s.id 
+        join slot s on gc.slot_id = s.id 
         join membership m on gc.member_id = m.id 
         join game_submission gs on m.id = gs.member_id and m.year = gs.year 
         join "user" u on m.user_id = u.id 
-      where gc.year = ${year} 
+      where gc.year = ${year} and gc.game_id in (select g.id from "game" g where g.year = ${year} or (g.id >= 596 and g.id <= 604))
       order by u.id, gc.year, s.slot, gc.rank;`
     await queryToExcelDownload(query, res)
   } catch (err: any) {
