494a4a7aa4f4e1013576ad7a0556d7eba595c2f3
Edwin Voskamp <35160149+evoskamp@users.noreply.github.com>
2023-03-25 12:31:12 -0700

    Add subqueries with vouchers for members and GMs (#118)
    Resolves: #117

--- apps/acus/pages/api/reports/membershipReport.ts
+++ apps/acus/pages/api/reports/membershipReport.ts
@@ -34,30 +34,39 @@ export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiRespo
         where m.year = ${year}
         order by u.full_name`
       : `
-        select 
-          m.id as "Member Id",
-          u.full_name as "Full Name",
-          u.first_name as "First Name",
-          u.last_name as "Last Name",
-          u.email as "email",
-          coalesce(gm.isGm,false) as "isGM",
-          mc.times_attending as "Times Attending",
-          m.attending as "isAttending",
-          m.attendance as "Attendance",
-          m.volunteer as "isVolunteer",
-          m.message as "Message"
+      select 
+        m.id as "Member Id",
+        u.full_name as "Full Name",
+        u.first_name as "First Name",
+        u.last_name as "Last Name",
+        u.email as "email",
+        coalesce(gm.isGm,false) as "isGM",
+        gmc.times_gming as "timesGMing",
+        mc.times_attending as "Times Attending",
+        m.attending as "isAttending",
+        m.attendance as "daysAttending",
+        m.attendance as "Attendance",
+        m.volunteer as "isVolunteer",
+        m.message as "Message"
+        from membership m 
+        join "user" u on m.user_id = u.id 
+        left join (select distinct m.id, true as isGm
+            from membership m
+              join game_assignment ga on ga.member_id = m.id
+            where m.year = ${year} and ga.gm < 0
+        ) gm on gm.id = m.id
+        join hotel_room h on m.hotel_room_id = h.id
+        left join (select m.user_id, count(m.user_id) as times_attending 
           from membership m 
-          join "user" u on m.user_id = u.id 
-          left join (select distinct m.id, true as isGm
-              from membership m
-                join game_assignment ga on ga.member_id = m.id
-              where m.year = 2022 and ga.gm < 0
-          ) gm on gm.id = m.id
-          join hotel_room h on m.hotel_room_id = h.id
-          left join (select m.user_id, count(m.user_id) as times_attending 
-            from membership m 
-            group by m.user_id
-          ) mc on u.id = mc.user_id
+          group by m.user_id
+        ) mc on u.id = mc.user_id
+        left join (select ga.member_id, ga.year, count(ga.member_id) as times_gming 
+          from game_assignment ga
+          join game g
+          on ga.game_id = g.id 
+          where ga.gm < 0 and g.slot_id >=1 and g.slot_id <= 8
+          group by ga.member_id, ga.year
+          ) gmc on m.id = gmc.member_id AND m.year = gmc.year
         where m.year = ${year}
         order by u.full_name;
         `
