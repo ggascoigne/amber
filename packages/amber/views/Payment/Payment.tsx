a6b2a913332de16d59604d5579d37ad9374f7ac4
Edwin Voskamp <github.com@voskamp.org>
2024-01-13 21:12:51 -0800

    Back out changes to Payment view (#147)
    Undid changes to packages/amber/veiws/Payment/Payment.tsx as they
    ended up in with issue #144 on that branch.
    
    Resolves: #148

--- packages/amber/views/Payment/Payment.tsx
+++ packages/amber/views/Payment/Payment.tsx
@@ -10,7 +10,7 @@ import { fetchPostJSON } from './fetchUtils'
 
 import { useGraphQL, GetUserByIdDocument } from '../../client'
 import { ContactEmail } from '../../components'
-import { useGetStripe, useConfiguration, useUser } from '../../utils'
+import { useGetStripe, useUser } from '../../utils'
 
 export const Payment: React.FC = () => {
   const [stripe] = useGetStripe()
@@ -33,10 +33,6 @@ export const Payment: React.FC = () => {
 
   // console.log('Payment', { paymentIntent })
 
-  const configuration = useConfiguration()
-  const acus = configuration.numberOfSlots === 8
-  const acnw = !acus
-
   return (
     <Page title='Make Payment'>
       {stripe && paymentIntent?.client_secret && user.userId ? (
@@ -57,7 +53,7 @@ export const Payment: React.FC = () => {
       ) : (
         <Loader />
       )}
-      {acnw && balance < 0 ? (
+      {balance < 0 ? (
         <>
           <DialogContentText sx={{ pt: 2 }}>
             Alternatively, write a check for <strong>${Math.max(0 - balance, 0)}</strong> made out to{' '}
