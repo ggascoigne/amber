f0e13da73e753d467e5ba12b940e7cc50f136b9c
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2024-08-11 15:28:49 -0700

    exclude .next folder from the deploy #2

--- apps/acnw/next.config.js
+++ apps/acnw/next.config.js
@@ -37,7 +37,7 @@ const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   experimental: {
     outputFileTracingExcludes: {
-      '*': ['node_modules/@swc/**', '.next/**'],
+        '*': ['node_modules/@swc/**', './.next/**'],
     },
   },
   headers,
