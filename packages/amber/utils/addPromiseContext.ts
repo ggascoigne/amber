0ca13db911e0d92b54eb3b7c92c44ba74496bc26
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2024-08-11 09:23:35 -0700

    Dependency upgrades

---
+++ packages/amber/utils/addPromiseContext.ts
@@ -0,0 +1,11 @@
+export const addContext = async <T, C = any>(context: C, promise: Promise<T>) => {
+  try {
+    // mostly this exists to ensure that in a mass of promise results we don't have
+    // to rely on the backend to tell us what the call was.
+    const result = await promise
+    return await Promise.resolve({ context, value: result })
+  } catch (reason) {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    return await Promise.reject({ context, value: reason })
+  }
+}
