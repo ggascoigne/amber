1694eccc9936b6446c1aed692ec4d9ccb38e4740
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2025-05-12 08:11:45 -0700

    try another approach to get the cert availabe to prisma on vercel

--- packages/server/src/db.ts
+++ packages/server/src/db.ts
@@ -1,15 +1,30 @@
+import fs from 'fs'
+import path from 'path'
+
 import { env, isDev } from '@amber/environment'
+import { certs } from 'database/shared/dbCerts'
 
 // eslint-disable-next-line import/no-relative-packages
 import { PrismaClient } from './generated/prisma/client'
 
-const createPrismaClient = (type: 'ADMIN' | 'USER') =>
-  new PrismaClient({
+const filename = '/tmp/rds-cert.pem'
+
+const createPrismaClient = (type: 'ADMIN' | 'USER') => {
+  if (env.DATABASE_SSL_CERT) {
+    const certName = path.basename(env.DATABASE_SSL_CERT ?? '', '.pem')
+    // eslint-disable-next-line no-prototype-builtins
+    if (!certs.hasOwnProperty(certName)) {
+      throw new Error(`SSL was enabled, but the named cert, '${certName}' is not installed.`)
+    }
+    fs.writeFileSync(filename, certs[certName]!)
+  }
+  return new PrismaClient({
     log: isDev ? ['query', 'error', 'warn'] : ['error'],
     datasources: {
       db: { url: type === 'ADMIN' ? env.ADMIN_DATABASE_URL : env.DATABASE_URL },
     },
   })
+}
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined
