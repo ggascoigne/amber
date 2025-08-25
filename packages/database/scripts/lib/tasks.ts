3337bb1d91473459bd14f59d510706cbf944a35a
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2025-08-24 18:12:50 -0700

    fix lint error

--- packages/database/scripts/lib/tasks.ts
+++ packages/database/scripts/lib/tasks.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
-import path from 'path'
 import os from 'os'
+import path from 'path'
 
 import { parsePostgresConnectionString, safeConnectionString } from '@amber/environment'
 import { type EnvType, processEnv } from '@amber/environment'
--- packages/server/src/db.ts
+++ packages/server/src/db.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
-import path from 'path'
 import os from 'os'
+import path from 'path'
 
 import { env, isDev } from '@amber/environment'
 import { certs } from 'database/shared/dbCerts'
