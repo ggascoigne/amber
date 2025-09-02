8bd35060a705cbb1df3dac38628756e77755d550
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2025-09-02 15:55:34 -0700

    fix cookie name to avoid decryption crash

--- packages/server/src/auth/auth0.ts
+++ packages/server/src/auth/auth0.ts
@@ -11,6 +11,7 @@ export const auth0 = new Auth0Client({
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   secret: process.env.AUTH0_SECRET,
   appBaseUrl,
+  session: { cookie: { name: 'appSession-v4' } },
   routes: {
     login: '/api/auth/login',
     callback: '/api/auth/callback',
