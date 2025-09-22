4d681c1b334b1d7eaa49dae09efb8ee81fe35730
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2025-09-22 12:27:08 -0700

    Fix Game Assignment editing (#218)
    The logic was rather simplistic and the prisma delete of the composite
    index was broken.  Both improved.

--- packages/environment/src/env.ts
+++ packages/environment/src/env.ts
@@ -12,7 +12,9 @@ export const isDev = process.env.NODE_ENV !== 'production'
 export const isTest = process.env.NODE_ENV === 'test'
 export const dbEnv = process.env.DB_ENV
 
-if (process.env.NODE_ENV !== 'production') {
+const isNodeRuntime = typeof process !== 'undefined' && !!process.versions?.node
+
+if (isNodeRuntime && process.env.NODE_ENV !== 'production') {
   const { dirname } = getPaths(import.meta.url)
   const envFilename = process.env.ENV_FILENAME ?? '.env'
   if (!dbEnv || !['acnw', 'acus'].includes(dbEnv)) {
