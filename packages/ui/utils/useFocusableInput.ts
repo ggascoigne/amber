51b1179a119e89083bc3d4b0e7a88029057a2ac9
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2026-03-15 11:06:58 -0700

    Dependency updates

--- packages/ui/utils/useFocusableInput.ts
+++ packages/ui/utils/useFocusableInput.ts
@@ -2,14 +2,14 @@ import { useEffect, useRef } from 'react'
 
 // use to assign focus to a field that might not be visible as the form mounts
 
-export function useFocusableInput(shouldFocus: boolean) {
+export const useFocusableInput = (shouldFocus: boolean) => {
   const inputRef = useRef<HTMLInputElement | null>(null)
   const setInputRef = (instance: HTMLInputElement | null) => {
     inputRef.current = instance
   }
 
   useEffect(() => {
-    let timeout: NodeJS.Timeout
+    let timeout: ReturnType<typeof setTimeout> | undefined
     if (shouldFocus) {
       timeout = setTimeout(() => {
         inputRef.current?.focus()
